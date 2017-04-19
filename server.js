98c39056c08eb83697cb8cc399159d8a56543c32
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2017-04-19 14:56:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -171,6 +171,10 @@ app.get('/test-db',function(req,res){
     //make a request
     var name='hari2';
     var pass='value';
+    pool.query("select * from Restaurant",function(err,result){
+        res.send(result);
+    });
+    /*
     pool.query("insert into Users(name,password) values('"+name+"','"+pass+"')",function(err,result){
         if(err){
             res.status(500).send(err.toString());
@@ -181,6 +185,7 @@ app.get('/test-db',function(req,res){
         }
     });
     //respond with data
+    */
 });
 
 function hash(input,salt){
