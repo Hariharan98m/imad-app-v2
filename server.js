5bd6331697a1a173ce477a908eb5f6152091bdab
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2017-04-29 22:49:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,13 +128,13 @@ function f(data){
               <br>
               <div>
               <input type='submit' value='Daily Menu' id='subbtn2' style="font-family:calibri;
-    font-size: 15px;
+    font-size: 20px;
     color: #632f63;
     background:white"/>
     <br>
               ${menu}
               <br>
-              <input type='submit' value='Dishes' id='subbtn3' style="font-family:calibri;background:white"/>
+              <input type='submit' value='Dishes' id='subbtn3' style="font-family:calibri;font-size:20px;background:white"/>
               ${list}
               </div>
               <br>
